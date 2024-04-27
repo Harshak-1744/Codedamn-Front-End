7db07154355109df0ab38e4b7edbadb6ad0548b3
Harshak-1744 <harshavardhan.hv36@gmail.com>
2024-04-27 20:58:53 +0530

    Update 1.js

---
+++ HTML/JS/1.js
@@ -0,0 +1,9 @@
+// Objective-1 : Create a JavaScript object to represent a book.
+
+const Book = {
+    title: 'How To Raise Your Own Salary',
+    author: 'Napoleon Hill',
+    yearPublished: 2020
+}
+
+console.log(Book.title + " by " + Book.author + ", published in " + Book.yearPublished);
