f4ac07d8d82bcbfb8f61e978efbcae16a8b51822
Harshak-1744 <harshavardhan.hv36@gmail.com>
2024-04-30 23:33:04 +0530

    modified:   HTML/JS/4/4.html 	modified:   HTML/JS/4/4.js

--- HTML/JS/4/4.js
+++ HTML/JS/4/4.js
@@ -1,1 +1,11 @@
 //Objective:  Create a basic webpage that changes its theme
+
+function DarkTheme() {
+    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector('#paragraph').style.color = 'white';
+}
+
+function LightTheme() {
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('#paragraph').style.color = 'black';
+}
