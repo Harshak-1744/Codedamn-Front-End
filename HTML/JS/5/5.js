f95f6f2da2c805d98167267d44f5389edb5be639
Harshak-1744 <harshavardhan.hv36@gmail.com>
2024-05-02 11:59:15 +0530

    modified:   HTML/JS/5/5.css 	modified:   HTML/JS/5/5.html 	modified:   HTML/JS/5/5.js

--- HTML/JS/5/5.js
+++ HTML/JS/5/5.js
@@ -1,5 +1,14 @@
+function DarkTheme() {
+    document.querySelector('body').style.backgroundColor = "#28282B"
+}
+
+function LightTheme() {
+    document.querySelector('body').style.backgroundColor = "White"
+}
+
 function updateImage(src, description) {
     const mainImage = document.querySelector('#mainImage');
     mainImage.src = src;
     document.querySelector('#description').textContent = description;
 }
+
