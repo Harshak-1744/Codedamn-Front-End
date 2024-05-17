1040fd4d7c15674a027eff237707a62d8742586f
Harshak-1744 <harshavardhan.hv36@gmail.com>
2024-05-17 12:49:05 +0530

    modified:   HTML/JS/8/8.js

--- HTML/JS/8/8.js
+++ HTML/JS/8/8.js
@@ -1,1 +1,13 @@
 fetch('./data.json')
+.then((response) => {
+    if(!response.ok){
+        throw new Error(`${response.status}`)
+    }
+    return response.json();
+})
+.then((data) => {
+    console.log(data)
+    document.getElementById("dummy").innerHTML = `<pre>${JSON.stringify(data,null,2)}</pre>`;
+}).catch((error) => {
+    console.error(error)
+})
