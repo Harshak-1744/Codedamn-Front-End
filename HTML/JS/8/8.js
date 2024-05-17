134e1ef85e7e23a163747bd5332be6ec3a05d10b
Harshak-1744 <harshavardhan.hv36@gmail.com>
2024-05-17 11:36:22 +0530

    modified:   HTML/JS/8/8.html 	modified:   HTML/JS/8/8.js

--- HTML/JS/8/8.js
+++ HTML/JS/8/8.js
@@ -1,13 +1,39 @@
+let jsonData = [];
+
 fetch('./data.json')
-.then((response) => {
+.then(response => {
     if (!response.ok) {
-        throw new Error(`${response.status}`);
+        throw new Error(`HTTP error! Status: ${response.status}`);
     }
     return response.json();
 })
-.then((data) => {
-    const container = document.getElementById("dummy");
-    container.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;  
-}).catch((error) => {
+.then(data => {
+    jsonData = data.books;  
+    console.log("Books loaded:", jsonData);  
+})
+.catch(error => {
+    console.error('Fetching error:', error);
     document.getElementById("dummy").innerHTML = `Error: ${error}`;
 });
+
+function searchData() {
+    const searchTerm = document.getElementById("searchInput").value.toLowerCase();
+    console.log("Search term:", searchTerm); 
+
+    
+    const results = jsonData.filter(book =>
+        book.title.toLowerCase().includes(searchTerm) ||
+        book.author.toLowerCase().includes(searchTerm)
+    );
+
+    console.log("Search results:", results);  
+    const container = document.getElementById("dummy");
+    container.innerHTML = `<pre>${JSON.stringify(results, null, 2)}</pre>`;
+
+    if(results.stock > 0) {
+        console.log(results.stock)
+    }
+    container.innerHTML = `<pre>${JSON.stringify(results, null, 2)}</pre>`;
+}
+
+
