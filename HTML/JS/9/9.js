0d58c0348535959920efc771fdbb9e13a916a2d0
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2024-05-28 10:17:38 +0530

    Update 9.js

--- HTML/JS/9/9.js
+++ HTML/JS/9/9.js
@@ -1,3 +1,27 @@
-const h1 = document.createElement('h1');
-h1.innerHTML = "Kiddo";
-document.body.appendChild(h1);
+async function convertCurrency() {
+  try {
+    let amount = document.getElementById("amount").value;
+    const From_Currency = `USD`;
+    const To_Currency = `INR`;
+
+    const url = `https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=${From_Currency}&to=${To_Currency}&amount=${amount}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'bb809bbd49msh28b9b76fb03074ep14045fjsn37c3992cbaf4',
+        'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
+      }
+    };
+
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`${response.status}`);
+    }
+
+    const data = await response.json();
+    document.getElementById("result").innerHTML = `Result: ${data.result}`;
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
