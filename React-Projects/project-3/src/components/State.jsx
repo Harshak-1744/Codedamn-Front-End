172be66725762da31a25014c8927ed7f8b86eda3
Harshak-1744 <harshavardhan.hv36@gmail.com>
2024-06-06 22:06:42 +0530

    Update State.jsx

--- React-Projects/project-3/src/components/State.jsx
+++ React-Projects/project-3/src/components/State.jsx
@@ -1,18 +1,14 @@
 import React, { useState } from "react";
 
-export default function State(){
-    let [counter,useCounter] = useState(0);
+export default function State() {
+
+    const [counter, setCounter] = useState(0);
 
     return (
-        <>
-            <h1>Counter : {counter}</h1>
-            <button onClick={()=>{
-                useCounter(counter+1)
-            }}>Increment</button>
-             <button onClick={()=>{
-                useCounter(counter-1)
-            }}>Decrement</button>
+        <>       
+            <h1>Counter: {counter}</h1>
+            <button onClick={() => setCounter(counter + 1)}>Increment</button>
+            <button onClick={() => setCounter(counter - 1)}>Decrement</button>
         </>
     );
-
 }
