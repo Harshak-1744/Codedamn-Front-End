3e0db21c74e248727932cbee8a2a53bad7a14aa8
Harshak-1744 <harshavardhan.hv36@gmail.com>
2024-06-14 15:36:16 +0530

    modified:   React-Projects/project-6/public/index.html 	modified:   React-Projects/project-6/src/components/Game.jsx

--- React-Projects/project-6/src/components/Game.jsx
+++ React-Projects/project-6/src/components/Game.jsx
@@ -2,7 +2,13 @@ import React, {useState} from 'react';
 
 
 export default function () {
+
+    const [game, setGame] = useState('rock');
+
     return <>
-        <h1>This is the demo</h1>
+        <h1>{game}</h1>
+        <button onClick={() => {
+            setGame(game)
+        }}>Click Me</button>
     </>
 }
